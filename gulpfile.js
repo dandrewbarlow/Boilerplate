5b5415327184dc458f9ffe2201a61ccaa0d0b6e7
Andrew Barlow <dandrewbarlow@gmail.com>
2020-02-27 15:50:46 -0700

    added autoprefix and gulp-sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,35 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
 
-// compile scss into css
 function style() {
-  // where is the scss file
-  return gulp.src('./scss/**/*.scss')
-  // pass through sass compiler
-  .pipe(sass())
-  // where to save compiled css
-  .pipe(gulp.dest('./css'))
-  // stream changes to all browsers
-  .pipe(browserSync.stream());
+return gulp.src('./scss/**/*.scss')
+.pipe(sourcemaps.init())
+//default style indented based on nesting
+.pipe(sass({outputStyle: 'nested'}))
+//indented but not based on nesting
+// .pipe(sass({outputStyle: 'expanded'}))
+//rules are one line
+// .pipe(sass({outputStyle: 'compact'}))
+//production css (minified) one line no white space
+// .pipe(sass({outputStyle: 'compressed'}))
+.pipe(browserSync.stream())
+.pipe(autoprefixer({
+    cascade: false
+}))
+.pipe(sourcemaps.write('.'))
+.pipe(gulp.dest('./css'));
 }
 
-
-function watch() {
-  // init local server
-  browserSync.init({
-    server : {
-      baseDir: './'
-    }
-  });
-  // watch css
-  gulp.watch('./scss/**/*.scss', {ignoreInitial: false}, style);
-  // watch html
-  gulp.watch('./*.html').on('change', browserSync.reload);
-  // watch js
-  gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+function watch() { browserSync.init({
+server: {
+baseDir: './' }
+});
+gulp.watch('./scss/**/*.scss', {ignoreInitial: false}, style);
+gulp.watch('./*.html').on('change', browserSync.reload);
+gulp.watch('./js/**/*.js').on('change', browserSync.reload);
 }
 
 exports.style = style;
